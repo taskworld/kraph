7346017b1b3bf20845edf16aad73a759b4e04518
Verachad Wongsawangtham <birth24987@gmail.com>
2017-05-21 20:30:52 +0700

    Split the print logic into sealed class

--- core/src/main/kotlin/me/lazmaid/kraph/lang/GraphQLNode.kt
+++ core/src/main/kotlin/me/lazmaid/kraph/lang/GraphQLNode.kt
@@ -1,9 +1,5 @@
 package me.lazmaid.kraph.lang
 
-/**
- * Created by VerachadW on 10/2/2016 AD.
- */
-
 abstract internal class GraphQLNode {
     var level = 0
 
@@ -12,74 +8,95 @@ abstract internal class GraphQLNode {
     fun getIndentString(level: Int) = "  ".repeat(level)
     fun getNewLineString(prettyFormat: Boolean) = if (prettyFormat) "\n" else "\\n"
 
-    private fun printEscaped(value: Any?, prettyFormat: Boolean) =
-            if (prettyFormat) {
-                "\"$value\""
-            } else {
-                "\\\"$value\\\""
+    fun print(value: Map<String, Any?>, prettyFormat: Boolean): String {
+        return value.entries.foldIndexed("") { index, acc, (k, v)->
+            var newAcc = acc + "$k: ${convertToDataEntry(v).print(prettyFormat)}"
+            if (index != value.entries.size - 1) {
+                newAcc += ", "
             }
-
-    fun print(map: Map<String, Any>, prettyFormat: Boolean) =
-            map.entries.foldIndexed("") { index, acc, entry ->
-                var string = acc + "${entry.key}: ${
-                when (entry.value) {
-                    is String -> {
-                        printEscaped(entry.value, prettyFormat)
-                    }
-                    is List<*> -> {
-                        val valueList = entry.value as List<*>
-                        valueList.map {
-                            printEscaped(it as String, prettyFormat)
-                        }
-                    }
-                    is KraphDataObject -> {
-                        (entry.value as KraphDataObject).print()
-                    }
-                    else -> {
-                        entry.value
-                    }
-                }}"
-                if (index != map.size - 1) {
-                    string += ", "
-                }
-                string
-            }
-}
-
-class KraphDataObject : HashMap<String, Any>() {
-    private fun printList(list: List<Any?>): String {
-        list.fold("") { acc, item ->
-            when (item) {
-                is KraphDataObject -> {
-                    item.print()
-                }
-                is List<*> -> {
-                    printList(item)
-                }
-                else -> {
-                    item
-                }
-            }
-            acc
+            newAcc
         }
     }
 
-    fun print() =
-            this.entries.forEach {
-                when (it.value) {
-                    is String -> {
-                        it.value
-                    }
-                    is List<*> -> {
-                        printList(it as List<Any?>)
-                    }
-                    is KraphDataObject -> {
-                        (it.value as KraphDataObject).print()
-                    }
-                    else {
-                    }
+    private fun convertToArrayData(value: List<*>): DataEntry.ArrayData {
+        return DataEntry.ArrayData(value.map(this::convertToDataEntry))
+    }
+
+    private fun convertToObjectData(map: Map<String, *>): DataEntry.ObjectData {
+        return DataEntry.ObjectData(map.map {
+            it.key to convertToDataEntry(it.value)
+        })
+    }
+
+    private fun convertToDataEntry(value: Any?) =
+            when(value) {
+                is String -> {
+                    DataEntry.StringData(value)
+                }
+                is Int -> {
+                    DataEntry.NonDecimalNumberData(value.toLong())
+                }
+                is Long -> {
+                    DataEntry.NonDecimalNumberData(value)
+                }
+                is Float, Double -> {
+                    DataEntry.DecimalNumberData(value as Double)
+                }
+                is List<*> -> {
+                    convertToArrayData(value)
+                }
+                is Map<*, *> -> {
+                    convertToObjectData(value as Map<String, *>)
+                }
+                else -> {
+                    throw RuntimeException("")
                 }
             }
 }
 
+internal sealed class DataEntry {
+    abstract fun print(prettyFormat: Boolean): String
+
+    class NonDecimalNumberData(val value: Long) : DataEntry() {
+        override fun print(prettyFormat: Boolean) = value.toString()
+    }
+
+    class DecimalNumberData(val value: Double) : DataEntry() {
+        override fun print(prettyFormat: Boolean) = value.toString()
+    }
+
+    class StringData(val value: String) : DataEntry() {
+        override fun print(prettyFormat: Boolean) =
+                if (prettyFormat) {
+                    "\"$value\""
+                } else {
+                    "\\\"$value\\\""
+                }
+    }
+
+    class ArrayData(val values: List<DataEntry>) : DataEntry() {
+        override fun print(prettyFormat: Boolean) =
+                values.foldIndexed("[") { index, acc, item ->
+                    var newAcc = acc + item.print(prettyFormat)
+                    if (index != values.size - 1) {
+                        newAcc += ", "
+                    } else {
+                        newAcc += "]"
+                    }
+                    newAcc
+                }
+    }
+
+    class ObjectData(val values: List<Pair<String, DataEntry>>) : DataEntry() {
+        override fun print(prettyFormat: Boolean) =
+                values.foldIndexed("{") { index, acc, (k, v) ->
+                    var newAcc = acc + "$k: ${v.print(prettyFormat)}"
+                    if (index != values.size - 1) {
+                        newAcc += ", "
+                    } else {
+                        newAcc += "}"
+                    }
+                    newAcc
+                }
+    }
 }
